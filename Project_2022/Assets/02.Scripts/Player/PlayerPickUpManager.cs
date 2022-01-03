a63403934f7bee9a350d5b29308cf212d9c62a5c
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-01-04 00:40:22 +0900

    상호작용 가능 시 손모양으로 마우스 커서가 바뀌도록 스크립트 변경

--- Project_2022/Assets/02.Scripts/Player/PlayerPickUpManager.cs
+++ Project_2022/Assets/02.Scripts/Player/PlayerPickUpManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerPickUpManager : MonoBehaviour
 {
     [SerializeField]
     private Inventory inventory;
     [SerializeField]
-    private GameObject pickUpTxt;
+    private Image mouseImage;
+    [SerializeField]
+    private Sprite canUseSprite;
+    [SerializeField]
+    private Sprite baseSprite;
+
 
     public void CanPickUpItem(Item curItem)
     {
@@ -15,13 +21,24 @@ public class PlayerPickUpManager : MonoBehaviour
         {
             inventory.PickUpItem(curItem.itemInfo);
             curItem.gameObject.SetActive(false);
+            ShowPickUpText(false);
+            return;
         }
         ShowPickUpText(true);
     }
 
     public void ShowPickUpText(bool isItemOn)
     {
-        pickUpTxt.gameObject.SetActive(isItemOn);
+        if(isItemOn)
+        {
+            mouseImage.rectTransform.sizeDelta = new Vector2(64, 64);
+            mouseImage.sprite = canUseSprite;
+        }
+        else
+        {
+            mouseImage.rectTransform.sizeDelta = new Vector2(16, 16);
+            mouseImage.sprite = baseSprite;
+        }
     }
 
     
