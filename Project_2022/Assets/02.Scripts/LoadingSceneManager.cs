42225e67c67482f45235778ca105daea999f82dc
znshak <kjpark102312@naver.com>
2022-01-07 19:29:47 +0900

    로딩씬(임시), 타이틀 화면(임시) 틀작업

---
+++ Project_2022/Assets/02.Scripts/LoadingSceneManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneManager : MonoBehaviour
+{
+
+    [SerializeField] private Slider loadingSlider;
+    [SerializeField] private Button sceneChangeBtn;
+
+    private string sceneName = "Main";
+
+    AsyncOperation operation;
+
+    void Start()
+    {
+        StartCoroutine(LoadCoroutine());
+
+        sceneChangeBtn.onClick.AddListener(() =>
+        {
+            Debug.Log("씬 로드");
+            operation.allowSceneActivation = true;
+        });
+    }
+
+    IEnumerator LoadCoroutine()
+    {
+        operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        float timer = 0f;
+        while (!operation.isDone)
+        {
+            yield return null;
+
+            timer += Time.deltaTime;
+            if (operation.progress < 0.9f)
+            {
+                loadingSlider.value = Mathf.Lerp(operation.progress, 1f, timer);
+                if (loadingSlider.value >= operation.progress)
+                    timer = 0f;
+            }
+            else
+            {
+                loadingSlider.value = Mathf.Lerp(loadingSlider.value, 1f, timer);
+                if (loadingSlider.value >= 0.99f)
+                    loadingSlider.gameObject.SetActive(false);
+                    
+            }
+        }
+    }
+}
---
+++ Project_2022/Assets/02.Scripts/UI/TitleUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleUI : MonoBehaviour
+{
+    [SerializeField] Button _newGameBtn;
+    [SerializeField] Button _continueBtn;
+    [SerializeField] Button _optionBtn;
+    [SerializeField] Button _exitBtn;
+
+    private string loadSceneName = "LoadingScene";
+
+    private void Start()
+    {
+        _newGameBtn.onClick.AddListener(() =>
+        {
+            // 로딩창 및 씬 이동 제어
+            SceneManager.LoadScene(loadSceneName);
+        });
+
+        _continueBtn.onClick.AddListener(() =>
+        {
+            // 나중 저장시스템이 나왔을 경우 저장된 게임 데이터 불러오기
+            SceneManager.LoadScene(loadSceneName);
+        });
+
+        _optionBtn.onClick.AddListener(() =>
+        {
+            // 옵션창 띄우기
+        });
+
+        _exitBtn.onClick.AddListener(() =>
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        });
+    }
+}
