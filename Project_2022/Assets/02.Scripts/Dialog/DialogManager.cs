8a2ca91b04f81a2b027db10da01b88302c591e14
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-01-15 22:04:24 +0900

    새로운 모델 추가 및 배치 수정

--- Project_2022/Assets/02.Scripts/Dialog/DialogManager.cs
+++ Project_2022/Assets/02.Scripts/Dialog/DialogManager.cs
@@ -2,10 +2,56 @@ using System.Collections;
 using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
+    [Header("상하좌우 빈 공간입니다")]
+    public FreeSpace freeSpace = new FreeSpace();
+    public int limitPanelCount = 0;
 
+    public DialogPanel panelPrefab = null;
+    List<DialogPanel> dialogs = new List<DialogPanel>();
+
+    RectTransform myRect = null;
+
+    private void Awake()
+    {
+        myRect = GetComponent<RectTransform>();
+    }
+
+    public void Refresh()
+    {
+
+    }
+
+    public void Resize()
+    {
+
+    }
+
+    public DialogPanel CreateDialogPanel()
+    {
+
+        if(dialogs.Count >= 1 && dialogs.Find(x => !x.gameObject.activeSelf))
+        {
+
+        }
+        else
+        {
+            
+        }
+
+        return null;
+    }
+
+    public class FreeSpace
+    {
+        public int left = 0;
+        public int right = 0;
+        public int up = 0;
+        public int down = 0;
+    }
 }
 
 [CreateAssetMenu(fileName = "DialogDatas", menuName = "ScriptableObject/Dialog")]
--- Project_2022/Assets/02.Scripts/Dialog/DialogPanel.cs
+++ Project_2022/Assets/02.Scripts/Dialog/DialogPanel.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using System;
 
 public class DialogPanel : MonoBehaviour
 {
-    
+    event Action _onDisable;
+
+    public int order;
 }
