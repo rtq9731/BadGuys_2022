a4c9a2ee24ae55545a7abeadace491a77a0bed7c
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-01-19 00:36:49 +0900

    스테이지 전환 시험 코드 추가

--- Project_2022/Assets/02.Scripts/StageManager/StageManager.cs
+++ Project_2022/Assets/02.Scripts/StageManager/StageManager.cs
@@ -6,11 +6,35 @@ using DG.Tweening;
 
 public class StageManager : MonoBehaviour
 {
-    [SerializeField] Text changeText;
+    [SerializeField] Text aiMsgText;
+    [SerializeField] Text stageMsgText;
     [SerializeField] Image panelHide;
+
+    private void Start()
+    {
+        StageChange();
+    }
 
     public void StageChange()
     {
-        panelHide.rectTransform.DOAnchorPosY(0, 1f);
+        panelHide.gameObject.SetActive(true);
+        stageMsgText.text = "";
+        aiMsgText.text = "";
+        panelHide.rectTransform.DOAnchorPosY(-panelHide.rectTransform.rect.height, 1f).OnComplete(()=>
+        {
+            stageMsgText.DOText("조심해요!!!", 1f);
+            stageMsgText.transform.DOShakePosition(10, 8f);
+            aiMsgText.DOText("M.A.M : 새로운 기억 발견, 장소 재구성 중. . .", 5f).OnComplete(() =>
+            {
+                aiMsgText.text = " ";
+                aiMsgText.DOText("M.A.M : 새 기억으로 업데이트된 장소로 이동합니다.", 3f).OnComplete(() =>
+                {
+                    panelHide.rectTransform.DOAnchorPosY(panelHide.rectTransform.rect.height / 2, 0.5f).OnComplete(() =>
+                    {
+                        panelHide.gameObject.SetActive(false);
+                    });
+                });
+            });
+        });
     }
 }
