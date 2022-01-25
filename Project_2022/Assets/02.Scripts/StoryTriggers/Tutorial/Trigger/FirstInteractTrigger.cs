a984925ba41f91a550dee99826fb52e6537bd13f
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-01-25 14:40:18 +0900

    사라졌던 트리거 재추가

---
+++ Project_2022/Assets/02.Scripts/StoryTriggers/Tutorial/Trigger/FirstInteractTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Triggers.Tutorial
+{
+    public class FirstInteractTrigger : StoryTrigger
+    {
+        public override void OnTriggered()
+        {
+            DialogManager.Instance.SetDialaogs(datas.GetDialogs());
+        }
+    }
+}
