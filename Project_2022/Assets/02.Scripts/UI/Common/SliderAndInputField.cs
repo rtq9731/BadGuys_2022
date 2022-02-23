1a08bd5802f58d5f3db539b94661909dfbbac083
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-02-23 15:06:58 +0900

    옵션 관련 디테일 수정

---
+++ Project_2022/Assets/02.Scripts/UI/Common/SliderAndInputField.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderAndInputField : MonoBehaviour
+{
+    [SerializeField] float max = 100;
+    [SerializeField] float min = 0;
+
+    [SerializeField] Slider inputSlider;
+    [SerializeField] InputField inputField;
+
+    public event Action<float> onValueChange;
+
+    private void Start()
+    {
+        inputSlider.onValueChanged.AddListener(onValueChanged);
+        inputField.onValueChanged.AddListener(onValueChanged);
+    }
+
+    private void onValueChanged(float num)
+    {
+        inputField.onValueChanged.RemoveListener(onValueChanged);
+
+        num = Clamp(num * max);
+        inputField.text = num.ToString();
+        inputSlider.value = num / max;
+
+        onValueChange?.Invoke(num);
+        inputField.onValueChanged.AddListener(onValueChanged);
+    }
+
+    private void onValueChanged(string str)
+    {
+        inputSlider.onValueChanged.RemoveListener(onValueChanged);
+
+        if (float.TryParse(str, out float result))
+        {
+            result = Clamp(result);
+
+            inputField.text = result.ToString("D2");
+            Debug.Log(result.ToString("D2"));
+            inputSlider.value = result / max;
+        }
+        else
+        {
+            inputField.text = (inputSlider.value * max).ToString("D2");
+        }
+
+        onValueChange?.Invoke(result);
+        inputSlider.onValueChanged.AddListener(onValueChanged);
+    }
+
+    private float Clamp(float num)
+    {
+        if (num > max)
+        {
+            num = max;
+        }
+
+        if (num < min)
+        {
+            num = min;
+        }
+
+        return num;
+    }
+}
