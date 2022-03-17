997c43d732ed25e36ca510a28fad19ce08aac07a
kjpark102312@naver.com <kjpark102312@naver.com>
2022-03-17 20:10:03 +0900

    색깔 퍼즐 버그수정

--- Project_2022/Assets/02.Scripts/Prop/Picture/PictureAnswerChecker.cs
+++ Project_2022/Assets/02.Scripts/Prop/Picture/PictureAnswerChecker.cs
@@ -14,24 +14,30 @@ public class PictureAnswerChecker : MonoBehaviour
 
     public bool CheckAnswer()
     {
+        int index = 0;
+        
         pictureCode = pictureInfo.GetPictureInfo();
         pictureCorrectCode = pictureAnswer.pieceColorCode;
-        for (int i = 0; i < pictureCode.Length; i++)
+
+        if (pictureCode[index] == pictureCorrectCode[index])
         {
-            if(pictureCode[i] == pictureCorrectCode[i])
+            if(pictureCode[index+1] == pictureCorrectCode[index+1])
             {
-                isCorrect = true;
-                Debug.Log(isCorrect);
+                if (pictureCode[index + 2] == pictureCorrectCode[index+2])
+                {
+                    isCorrect = true;
+                    Debug.Log(isCorrect);
+                    return isCorrect;
+                }
                 return isCorrect;
             }
-            else
-            {
-                isCorrect = false;
-                Debug.Log(isCorrect);
-                return isCorrect;
-            }
+            return isCorrect;
         }
-        Debug.Log(isCorrect);
-        return isCorrect;
+        else
+        {
+            isCorrect = false;
+            Debug.Log(isCorrect);
+            return isCorrect;
+        }
     }
 }
