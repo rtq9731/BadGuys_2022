1abc4a6c14b13c2d2cdd9140b9468b7c3aada8ae
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-03-29 15:27:09 +0900

    그림 채우는 퍼즐 적용중

---
+++ Project_2022/Assets/02.Scripts/InteractObj/Paint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paint : MonoBehaviour, IInteractAndGetItemObj
+{
+    public void Interact(ItemInfo itemInfo, GameObject taker)
+    {
+        throw new System.NotImplementedException();
+    }
+}
