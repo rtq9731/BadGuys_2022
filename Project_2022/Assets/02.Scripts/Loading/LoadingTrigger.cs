997ace700a92b0aeddba6492d5100e2944263053
kjpark102312@naver.com <kjpark102312@naver.com>
2022-03-29 10:56:28 +0900

    로비로 이동하는 시스템 구현

--- Project_2022/Assets/02.Scripts/Loading/LoadingTrigger.cs
+++ Project_2022/Assets/02.Scripts/Loading/LoadingTrigger.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LoadingTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static LoadingTrigger instance = null;
+
+    public static LoadingTrigger Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<LoadingTrigger>();
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private Image blindImage;
+
+    public void Ontrigger()
     {
-        
+        //다이얼로그나 연출을 다 본후에 실행하게
+        blindImage.DOFade(1, 1.2f).OnComplete(() =>
+        {
+            StartCoroutine(LoadLobbyStage());
+        });
+    }
+
+    IEnumerator LoadLobbyStage()
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync("LoadingStageScene");
+        async.allowSceneActivation = false;
+
+        float timer = 0f;
+        float loadingTime = 1f;
+
+        while (true)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= loadingTime)
+            {
+                SceneManager.LoadScene("MainStage_stage1", LoadSceneMode.Additive);
+                async.allowSceneActivation = true;
+                yield break;
+            }
+            yield return null;
+        }
     }
 }
