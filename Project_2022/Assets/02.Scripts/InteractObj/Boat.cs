ff6ddbc24e0f1b06022f28b142b168608360af74
kjpark102312@naver.com <kjpark102312@naver.com>
2022-04-05 21:53:41 +0900

    스테이지들 인벤토리 다시 연결 / 보트 버그 수정

--- Project_2022/Assets/02.Scripts/InteractObj/Boat.cs
+++ Project_2022/Assets/02.Scripts/InteractObj/Boat.cs
@@ -6,8 +6,6 @@ using DG.Tweening;
 public class Boat : MonoBehaviour, IInteractableItem
 {
     [SerializeField]
-    private GameObject inventory;
-    [SerializeField]
     private GameObject boatCam;
     [SerializeField]
     private GameObject mainCam;
@@ -21,25 +19,31 @@ public class Boat : MonoBehaviour, IInteractableItem
     private bool isCanInterct = true;
     private bool isGoToSun = false;
 
+    bool isPause = false;
+
     Animator[] anim;
     BoatCam boatCamera;
     private void Start()
     {
-        rectTrm = inventory.transform.GetChild(0).GetComponent<RectTransform>();
         originPos = transform.position;
-        boatCamera = boatCam.GetComponent<BoatCam>();
+        
         anim = GetComponentsInChildren<Animator>();
     }
     private void Update()
     {
-        if(GameManager.Instance.IsPause)
+        if(boatCam.activeSelf)
         {
-            boatCamera.enabled = false;
+            if (GameManager.Instance.IsPause)
+            {
+                boatCamera.enabled = false;
+                isPause = true;
+            }
+            else if(!GameManager.Instance.IsPause && isPause)
+            {
+                boatCamera.enabled = true;
+                isPause = false;
+            }
         }
-        else
-        {
-            boatCamera.enabled = true;
-        }    
     }
     public void Interact(GameObject taker)
     {
@@ -71,6 +75,7 @@ public class Boat : MonoBehaviour, IInteractableItem
     {
         boatCam.SetActive(true);
 
+        boatCamera = boatCam.GetComponent<BoatCam>();
         playerCam.GetComponentInParent<PlayerController>().enabled = false;
         isGoToSun = true;
         boatCamera.enabled = false;
