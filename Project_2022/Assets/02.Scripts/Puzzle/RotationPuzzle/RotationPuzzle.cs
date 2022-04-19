702cda2b710834db77cb86fc7bcdcd0b20192bc6
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-04-19 16:38:16 +0900

    아이템 정보 패널 교체 및 프리팹화 / 로테이션 퍼즐 디버깅

--- Project_2022/Assets/02.Scripts/Puzzle/RotationPuzzle/RotationPuzzle.cs
+++ Project_2022/Assets/02.Scripts/Puzzle/RotationPuzzle/RotationPuzzle.cs
@@ -75,27 +75,37 @@ public class RotationPuzzle : MonoBehaviour
 
     private void OnElementRotate()
     {
-        Debug.Log("0 : " + (int)elements[0].GetPictureRotationZ() % 360 + " == " + "1 : " + (int)elements[1].GetPictureRotationZ() % 360 + " = " + ((int)elements[0].GetPictureRotationZ() == (int)elements[1].GetPictureRotationZ()));
-        Debug.Log("0 : " + (int)elements[0].GetPictureRotationZ() % 360 + " == " + "2 : " + (int)elements[2].GetPictureRotationZ() % 360 + " = " + ((int)elements[0].GetPictureRotationZ() == (int)elements[2].GetPictureRotationZ()));
-        if (!((int)(elements[0].GetPictureRotationZ() % 360) == (int)(elements[1].GetPictureRotationZ() % 360)
-            && (int)(elements[0].GetPictureRotationZ() % 360) == (int)(elements[2].GetPictureRotationZ() % 360)))
+        float a, b, c;
+        a = elements[0].GetPictureRotationZ();
+        b = elements[1].GetPictureRotationZ();
+        c = elements[2].GetPictureRotationZ();
+
+        if (!((int)(ClampForRoationPuzzle(a)) == (int)(ClampForRoationPuzzle(b))
+            && (int)(ClampForRoationPuzzle(a)) == (int)(ClampForRoationPuzzle(c))))
             return;
 
         OnCompletePuzzle(elements[0].GetPictureRotationZ());
     }
 
+    private float ClampForRoationPuzzle(float rot)
+    {
+        rot = Mathf.Round(rot);
+        Debug.Log(rot);
+        if (rot <= 0)
+        {
+            return 360 + (rot % 360);
+        }
+        return rot % 360;
+    }
+
     private void OnCompletePuzzle(float destRot)
     {
+        isOn = false;
         vCamPuzzle.SetActive(true);
         completeSR.transform.rotation = Quaternion.Euler(new Vector3(0, 180, destRot));
         completeSR.gameObject.SetActive(true);
         completeSR.material.SetFloat("_DissolveAmount", 0f);
         UIManager._instance.OnCutSceneWithoutPause();
-
-        foreach (var item in elements)
-        {
-            item.DORotateToAnswer(destRot);
-        }
 
         completeSR.material.DOFloat(1, "_DissolveAmount", 3f).OnComplete(() => 
         {
