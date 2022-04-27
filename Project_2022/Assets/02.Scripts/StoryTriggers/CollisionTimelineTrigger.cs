b7b8599da6098e3df9e7ec9f1dda9ec10d05b86e
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-04-27 18:00:02 +0900

    모델 폴더 정리와 텍스쳐 정리 / 타임라인 시험중

---
+++ Project_2022/Assets/02.Scripts/StoryTriggers/CollisionTimelineTrigger.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Timeline;
+
+[RequireComponent(typeof(SignalReceiver))]
+public class CollisionTimelineTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject timeLineObj = null;
+    [SerializeField] SignalReceiver receiver = null;
+
+    [SerializeField] SignalAsset startSignal = null;
+    [SerializeField] SignalAsset endSignal = null;
+
+    UnityEvent _onTimelineStart = new UnityEvent();
+    UnityEvent _onTimelineEnd = new UnityEvent();
+
+    private void Awake()
+    {
+        receiver = GetComponent<SignalReceiver>();
+    }
+
+    private void Start()
+    {
+        _onTimelineStart.AddListener(OnStart);
+        _onTimelineEnd.AddListener(OnComplete);
+
+        receiver.AddReaction(startSignal, _onTimelineStart);
+        receiver.AddReaction(endSignal, _onTimelineEnd);
+    }
+
+    virtual public void OnStart()
+    {
+        Debug.Log("타임라인 시작!");
+        UIManager._instance.OnCutScene();
+    }
+
+    virtual public void OnComplete()
+    {
+        Debug.Log("타임라인 끝!");
+        UIManager._instance.OnCutSceneOverWithoutClearDialog();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            timeLineObj.gameObject.SetActive(true);
+        }
+    }
+}
