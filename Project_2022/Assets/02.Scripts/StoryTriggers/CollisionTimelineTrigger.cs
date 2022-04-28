8e3412b3bc8f2e9aaffa11e4e4f4a9ab8482cab7
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-04-28 11:00:34 +0900

    2챕터 납치 컷씬 제작

--- Project_2022/Assets/02.Scripts/StoryTriggers/CollisionTimelineTrigger.cs
+++ Project_2022/Assets/02.Scripts/StoryTriggers/CollisionTimelineTrigger.cs
@@ -7,11 +7,9 @@ using UnityEngine.Timeline;
 [RequireComponent(typeof(SignalReceiver))]
 public class CollisionTimelineTrigger : MonoBehaviour
 {
-    [SerializeField] GameObject timeLineObj = null;
+    [SerializeField] GameObject timeline = null;
+    [SerializeField] GameObject vCamCutScene = null;
     SignalReceiver receiver = null;
-
-    [SerializeField] SignalAsset startSignal = null;
-    [SerializeField] SignalAsset endSignal = null;
 
     UnityEvent _onTimelineStart = new UnityEvent();
     UnityEvent _onTimelineEnd = new UnityEvent();
@@ -21,24 +19,17 @@ public class CollisionTimelineTrigger : MonoBehaviour
         receiver = GetComponent<SignalReceiver>();
     }
 
-    private void Start()
-    {
-        _onTimelineStart.AddListener(OnStart);
-        _onTimelineEnd.AddListener(OnComplete);
-
-        receiver.AddReaction(startSignal, _onTimelineStart);
-        receiver.AddReaction(endSignal, _onTimelineEnd);
-    }
-
     virtual public void OnStart()
     {
         Debug.Log("타임라인 시작!");
         UIManager._instance.OnCutScene();
+        vCamCutScene.SetActive(true);
     }
 
     virtual public void OnComplete()
     {
         Debug.Log("타임라인 끝!");
+        vCamCutScene.SetActive(false);
         UIManager._instance.OnCutSceneOverWithoutClearDialog();
     }
 
@@ -46,7 +37,7 @@ public class CollisionTimelineTrigger : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            timeLineObj.gameObject.SetActive(true);
+            timeline.gameObject.SetActive(true);
         }
     }
 }
