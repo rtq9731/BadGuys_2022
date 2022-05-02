30bb5d10288084f6ed7d51a6dc8839577f81839f
gunbread20 <gunbread4413@gmail.com>
2022-05-02 21:18:36 +0900

    스크린 버그 수정

--- Project_2022/Assets/02.Scripts/Chapter1_Main/ComputerMain.cs
+++ Project_2022/Assets/02.Scripts/Chapter1_Main/ComputerMain.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class ComputerMain : MonoBehaviour, IInteractableItem
 {
     [SerializeField]
-    private GameObject screenCanvas;
+    private GameObject screenObj;
     [SerializeField]
     private AudioSource bootingSound;
 
     private void Awake()
     {
-        if (screenCanvas == null)
+        if (screenObj == null)
             Debug.LogWarning("스크린 캔버스 세팅 안되어있음");
         else
-            screenCanvas.SetActive(false);
+            screenObj.SetActive(false);
     }
 
     public void Interact(GameObject taker)
     {
-        screenCanvas.SetActive(!screenCanvas.activeSelf);
+        screenObj.SetActive(!screenObj.activeSelf);
     }
 
     public bool CanInteract()
