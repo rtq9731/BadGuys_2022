036230929c516f5ca788e8d68b69338b57e18bb3
gunbread20 <gunbread4413@gmail.com>
2022-05-25 16:34:59 +0900

    챕터3 트리거 추가

---
+++ Project_2022/Assets/02.Scripts/Chapter3/PatrolCheck.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolCheck : MonoBehaviour
+{
+    public static PatrolCheck Instanse;
+
+    public bool isPlayerIn;
+    public bool isDoorClose;
+
+    private void Awake()
+    {
+        if (Instanse == null)
+            Instanse = this;
+        else if (Instanse != this)
+            Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isPlayerIn = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isPlayerIn = false;
+    }
+
+    public bool IsHide()
+    {
+        return (isPlayerIn && isDoorClose);
+    }
+}
