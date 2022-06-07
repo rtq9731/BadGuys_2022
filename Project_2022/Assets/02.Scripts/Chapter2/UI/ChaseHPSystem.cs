34ea2cb7a8af6e79eac344946f0af4c02ecad16b
gunbread20 <gunbread4413@gmail.com>
2022-06-07 19:11:06 +0900

    챕터2 실패 UI 추가

--- Project_2022/Assets/02.Scripts/Chapter2/UI/ChaseHPSystem.cs
+++ Project_2022/Assets/02.Scripts/Chapter2/UI/ChaseHPSystem.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class ChaseHPSystem : MonoBehaviour
 {
     [SerializeField] Image handCuffImage = null;
-
+    [SerializeField] StageReStart restart;
     [SerializeField] float maxHp = 100;
     [SerializeField] float damage = 1f;
 
     float hp = 100f;
+    bool over = false;
+
     public float HP
     {
         get { return hp; }
@@ -24,6 +26,13 @@ public class ChaseHPSystem : MonoBehaviour
 
     private void Update()
     {
+        if (hp <= 0 && !over)
+        {
+            over = true;
+            restart.Detection();
+        }
+            
+        
         if(!GameManager.Instance.IsPause)
             hp -= Time.deltaTime * damage;
 
