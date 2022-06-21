ecf9568dad5b6eb968fb139e65602fc9e4e50efe
kjpark102312@naver.com <kjpark102312@naver.com>
2022-06-21 20:04:08 +0900

    캐비넷 사운드 추가

--- Project_2022/Assets/02.Scripts/Prop/Cabinet.cs
+++ Project_2022/Assets/02.Scripts/Prop/Cabinet.cs
@@ -9,9 +9,14 @@ public class Cabinet : MonoBehaviour, IInteractableItem
 
     Animator anim;
 
+    AudioSource audioSource;
+    
+    [SerializeField] AudioClip[] clip;
+
     private void Start()
     {
         anim = GetComponent<Animator>();
+        audioSource = GetComponentInParent<AudioSource>();
     }
 
     public void Interact(GameObject taker)
@@ -25,12 +30,15 @@ public class Cabinet : MonoBehaviour, IInteractableItem
         {
             this._isOpen = false;
             anim.SetTrigger("IsClose");
+            audioSource.clip = clip[1];
         }
         else
         {
             this._isOpen = true;
             anim.SetTrigger("IsOpen");
+            audioSource.clip = clip[0];
         }
+        audioSource.Play();
     }
 
     public bool CanInteract()
