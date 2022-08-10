0637383f48fda0e19f5db49b322d8cf6685bd249
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-08-10 20:33:34 +0900

    크레딧 UI 추가

--- Project_2022/Assets/02.Scripts/UI/Credit/CreditMove.cs
+++ Project_2022/Assets/02.Scripts/UI/Credit/CreditMove.cs
@@ -6,7 +6,17 @@ using UnityEngine.UI;
 public class CreditMove : MonoBehaviour
 {
     [SerializeField] RectTransform creditRect;
-    
+    [SerializeField] float moveSpeed = 5f;
+
+    bool isSetTimer = false;
+
+    float timer = 0f;
+
+    private void Start()
+    {
+        SetMoveCredit();
+    }
+
     public void SetMoveCredit()
     {
         StartCoroutine(CreditCroutine());
@@ -16,7 +26,7 @@ public class CreditMove : MonoBehaviour
     {
         while (creditRect.anchoredPosition.y <= creditRect.rect.height)
         {
-            creditRect.anchoredPosition += Vector2.up * Time.deltaTime;
+            creditRect.anchoredPosition += Vector2.up * Time.deltaTime * moveSpeed;
             yield return null;
         }
 
