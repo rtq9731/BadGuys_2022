969578e96e325e819368669e080e4c7a3f2784b8
kjpark102312@naver.com <kjpark102312@naver.com>
2022-08-11 19:20:55 +0900

    아이템 상호작용으로 변경

--- Project_2022/Assets/02.Scripts/Credit/InteractItems.cs
+++ Project_2022/Assets/02.Scripts/Credit/InteractItems.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractItems : MonoBehaviour, IPlayerMouseEnterHandler
+public class InteractItems : MonoBehaviour, IInteractableItem
 {
 
     public event System.Action OnPlayerMouseEnter = null;
 
-    void IPlayerMouseEnterHandler.OnPlayerMouseEnter()
+    public bool CanInteract()
+    {
+        if (!enabled || !gameObject.activeSelf)
+            return false;
+
+        return true;
+    }
+
+    public void Interact(GameObject taker)
     {
         OnPlayerMouseEnter?.Invoke();
     }
